cbb89722b18d8f58cf09c1bbcea4d7e1532582a2
ximo <ximoxd@gmail.com>
2019-02-28 16:36:26 +0100

    Añadido el GeneradorDeDatos

---
+++ src/main/java/clases/GeneradorDatos.java
@@ -0,0 +1,42 @@
+package clases;
+
+import es.uji.www.GeneradorDatosINE;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+
+public class GeneradorDatos
+{
+    ArrayList<Cliente> listaClientesPrueba = new ArrayList();
+    GeneradorDatosINE gen = new GeneradorDatosINE();
+    Cliente cli;
+
+    public GeneradorDatos() {}
+
+    private void generar() { for (int i = 0; i < 50; i++) {
+        String prov = gen.getProvincia();
+        String nombre = gen.getNombre();
+        Direccion dir = new Direccion(12004 + i, prov, gen.getPoblacion(prov));
+        String email = nombre + i + "@gmail.com";
+        LocalDateTime fecha = LocalDateTime.now();
+        if (i % 2 == 0) {
+            cli = new Cliente(nombre, gen.getNIF(), dir, email, fecha, i);
+        }
+        else {
+            cli = new Particular(nombre, gen.getApellido(), gen.getNIF(), dir, email, fecha, i);
+        }
+
+        listaClientesPrueba.add(cli);
+    }
+    }
+
+    /*
+    public static void main(String[] args) {
+        GeneradorDatos g = new GeneradorDatos();
+        g.generar();
+        for (Cliente cl : g.listaClientesPrueba) {
+            System.out.println(cl.toString());
+        }
+    }*/
+
+}
