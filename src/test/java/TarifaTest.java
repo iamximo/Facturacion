d1d868b3be73d00d9c4eabeb5a415632c2f8ec34
iamximo <ximoxd@gmail.com>
2019-05-10 19:47:03 +0200

    Añadido algun caso Test Tarifas

--- src/test/java/TarifaTest.java
+++ src/test/java/TarifaTest.java
@@ -25,6 +25,7 @@ public class TarifaTest {
     //Tarifa basica=20 - TarifaDia=28
     TarifaDia tar4=fabrica.getTarifaDia(tar1,28,LocalDate.now().getDayOfWeek());
 
+
     //Tarifa basica=20 - TarifaHora=15
     TarifaHora tar5=fabrica.getTarifaHora(tar1,15, LocalTime.now(),LocalTime.now().plusHours(1));
 
@@ -33,6 +34,9 @@ public class TarifaTest {
 
     //Tarifa basica=20 - TarifaDia=5 - TarifaHora=6
     TarifaHora tar7=fabrica.getTarifaHora(tar3,6,LocalTime.now(),LocalTime.now().plusHours(1));
+
+    //Tarifa basica=20 - TarifaDia=4
+    TarifaDia tar8=fabrica.getTarifaDia(tar1,4,LocalDate.ofYearDay(2019,1).getDayOfWeek());
 
 
     @Test
@@ -46,6 +50,7 @@ public class TarifaTest {
     public void getPrecioTarifaTest(){
         Llamada lla1=new Llamada("67", LocalDateTime.now(),6);
         Llamada lla2=new Llamada("67", LocalDateTime.now().plusHours(-1),6);
+        Llamada lla3=new Llamada("67", LocalDateTime.of(2019,1,1,0,0),6);
 
         assertEquals(5*6,tar3.getPrecioTarifa(lla1),0); // Se aplica tarifa de dia
         assertEquals(20*6,tar4.getPrecioTarifa(lla1),0); // Se aplica tarifa basica
@@ -55,6 +60,8 @@ public class TarifaTest {
         assertEquals(2*6,tar6.getPrecioTarifa(lla1),0); // Se aplica tarifa hora
         assertEquals(5*6,tar7.getPrecioTarifa(lla1),0); // Se aplica tarifa de dia
 
+        assertEquals(20*6,tar8.getPrecioTarifa(lla1),0); // Se aplica tarifa basica
+        assertEquals(4*6,tar8.getPrecioTarifa(lla3),0); // Se aplica tarifa de dia
 
 
 
